e1adae1490c6bb3ea9fc38a919ac65429042c377
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-12 22:53:31 -0300

    added permission to frontend access the api

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use((request, response, next) => {
+  response.setHeader('Access-Control-Allow-Cross-Origin', 'http://localhost:3000');
+  next();
+});
 app.use(routes);
 app.use((error, request, response, next) => {
   response.sendStatus(500);
