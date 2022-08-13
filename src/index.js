27c75dbfacc89405f16320ac91792d6b2dbebf86
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-08-12 22:26:33 -0300

    removed console.log of ERROR HANDLER

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ const port = 3001;
 app.use(express.json());
 app.use(routes);
 app.use((error, request, response, next) => {
-  console.log('##### ERROR HANDLER');
-  console.log(error);
   response.sendStatus(500);
 });
 
